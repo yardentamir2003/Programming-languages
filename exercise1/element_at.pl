57b834bdc9503ddcfcf8fecfd613a8b91931713c
Yarden <yardent9@gmail.com>
2026-03-28 17:12:26 +0300

    created files and finished 1

--- exercise1/element_at.pl
+++ exercise1/element_at.pl
@@ -1,8 +1,9 @@
 % Base case - X is the first element in the list
 element_at(X, [X|_], 1).
 
-% Recursion on list's tail
-element_at(X, [_|Tail], k) :-
-    K > 1
-    New_K = K - 1,
-    element_at(X, [Tail|_], New_K).
+% Recursion on lists tail
+element_at(X, [_|Tail], K) :-
+    K > 1,
+    New_K is K - 1,
+    element_at(X, Tail, New_K).
+
